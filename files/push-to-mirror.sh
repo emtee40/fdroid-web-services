7d9b0061a33cfcffd71cee91b2bb9b82b9d2bcbf
Hans-Christoph Steiner <hans@eds.org>
2023-10-03 11:52:34 +0200

    push-to-mirror.sh: lock timeout rather than instant fail, plus logging
    The lock check was set to fail immediately.  The lock is per-host, and
    setting the archive job to `0 0 * * *` and repo to `*/10 * * * *` meant
    that the repo job was beating the archive job, then the archive job just
    failed immediately.

--- files/push-to-mirror.sh
+++ files/push-to-mirror.sh
@@ -10,14 +10,21 @@
 #   restrict,command="rsync --server -logDtpre.iLsfx --log-format=X --delete --delay-updates . /path/to/htdocs/fdroid/"
 #
 set -e
+set -x
 
 section=$1
 test -n "$section"
 host=$2
 test -n "$host"
 
+if [ "$section" == "archive" ]; then
+    timeout=600
+else
+    timeout=10
+fi
+echo "Waiting $timeout seconds for lock..."
 (
-    flock --exclusive --nonblock 200
+    flock --exclusive --timeout $timeout 200
     echo "Pushing $section to $host at $(date):"
 
     # be super careful with the trailing slashes here! if one is wrong, it'll delete the entire section!
